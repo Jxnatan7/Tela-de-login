b01217f256d650e4108cf59abdff07e7d135566a
Jonatan Gabriel <jgabrieldesouzasantos@gmail.com>
2022-09-12 11:12:46 -0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const botao = document.getElementById('switch')
-
-const html = document.querySelector('html')
-
-botao.addEventListener('click', function() {
-  html.classList.toggle('light-mode')
- })
-
